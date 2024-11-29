6be0f960f6d3555ab40c0b14f778979369196f3b
Miguel <miguelg71921@gmail.com>
2024-11-28 16:57:42 -0800

    fixed old paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'llama-homeassistant-blog'; // Replace with your GitHub repo name
+const repoName = 'browser-use-blog'; // Replace with your GitHub repo name
 
 const nextConfig = {
   output: 'export',
